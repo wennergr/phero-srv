703e4ea2345c2b261abf99d9268eb3e1e5a2c4cb
Tobias Wennergren <wennergr@gmail.com>
2022-06-03 20:09:07 -0700

    Added support for calling upsteeam service

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	_ "embed"
+	"encoding/json"
 	"github.com/gin-gonic/gin"
+	"log"
 	"os"
 	"strings"
 )
@@ -10,6 +12,11 @@ import "net/http"
 
 //go:embed version.txt
 var version string
+
+type Ping_Response struct {
+	Message string `json:"message"`
+	Name    string `json:"name"`
+}
 
 func main() {
 	r := gin.Default()
@@ -20,6 +27,7 @@ func main() {
 	}
 
 	message := "Hello!"
+
 	if len(os.Args) > 2 {
 		message = strings.Join(os.Args[2:], " ")
 	}
@@ -29,12 +37,26 @@ func main() {
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
+			"name":    prefix,
 		})
 	})
 
 	api.GET("/hello", func(c *gin.Context) {
+		resp, err := http.Get("http://phero-hidden:8080/ping")
+
+		var ping_response = Ping_Response{"Unknown", "Unknown"}
+
+		if err == nil {
+			if err = json.NewDecoder(resp.Body).Decode(&ping_response); err == nil {
+				log.Println("Unable to decode data form hidden phero server.", err)
+			}
+		} else {
+			log.Println("Unable to make request to hidden phero server. ", err)
+		}
+
 		c.JSON(http.StatusOK, gin.H{
-			"message": message,
+			"message":  message,
+			"upstream": ping_response,
 		})
 	})
 
